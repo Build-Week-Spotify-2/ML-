79a17538f9ce6b425bf070192b7c4696d6187f99
modares <modareshadi@gmail.com>
2020-06-23 23:47:31 -0700

    cleaned the directory and made it more neat by creating folders for appropriate files

---
+++ methods/methods.py
@@ -0,0 +1,87 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+
+def get_spotify_token():
+    '''
+    This function will initiate the spotify token
+    return: spotipy object
+    '''
+
+    import spotipy
+    import spotipy.util as util
+    import sys
+
+    load_dotenv("../.env")
+    SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+    SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+    SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+    USERNAME = os.getenv("USERNAME")
+
+    token = util.prompt_for_user_token(
+        username=USERNAME,
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri=SPOTIPY_REDIRECT_URI
+    )
+
+    sp = spotipy.Spotify(auth=token)
+    return sp
+
+
+def load_model():
+    import pickle
+    import sklearn
+
+    dbfile = open('models/kmeans.pickl', 'rb')
+    model = pickle.load(dbfile)
+    dbfile.close()
+    return model
+
+
+def load_labled_spotify_songs():
+    return pd.read_csv("data/labled_songs_id.csv")
+
+
+def get_songs_audio_features(song_id_list, spotipy_obj):
+    temp_data = spotipy_obj.audio_features(song_id_list)
+    spotify_audio_features = pd.DataFrame(temp_data)
+
+    important_features = [
+        # "genre",  # spotify genre doesn't match with kaggle genre
+        # "popularity",  # don't know how to get it for the song, and it changes over time
+        # "key",  # This is categorical
+        # "mode",  # this is categorical
+        # "time_signature"  # not for now
+        "acousticness",
+        "danceability",
+        "energy",
+        "instrumentalness",
+        "liveness",
+        "loudness",
+        "speechiness",
+        "tempo",
+        "valence",
+    ]
+    return spotify_audio_features[important_features]
+
+
+def get_n_similar_songs(song_label, labled_songs, n, spotipy_obj):
+    '''
+    args:
+        song_label: the average song audio features
+        labled_songs: the dataframe that has all the
+            songs' cluster lables
+        n: number of required recommendations
+    
+    return:
+        list of recommended song ids
+    '''
+
+    same_cluster_songs = \
+        labled_songs[labled_songs["label"] == song_label]\
+        .loc[:, "track_id"]
+
+    list_of_recommended_songs = same_cluster_songs.head(n).to_list()
+    return list_of_recommended_songs
--- predictor.py
+++ predictor.py
@@ -1,115 +1,31 @@
-import pandas as pd
-import os
-from dotenv import load_dotenv
+from methods import methods
 from pdb import set_trace as st
 
 
 def predict(song_id_list, recommendation_count):
-    
-    def get_spotify_token():
-        '''
-        This function will initiate the spotify token
-        return: spotipy object
-        '''
 
-        import spotipy
-        import spotipy.util as util
-        import sys
+    # get spotipy token
+    spotipy_obj = methods.get_spotify_token()
 
-        load_dotenv()
-        SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
-        SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
-        SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
-        USERNAME = os.getenv("USERNAME")
+    # load ML model
+    kmeans_model = methods.load_model()
 
-        token = util.prompt_for_user_token(
-            username=USERNAME,
-            client_id=SPOTIPY_CLIENT_ID,
-            client_secret=SPOTIPY_CLIENT_SECRET,
-            redirect_uri=SPOTIPY_REDIRECT_URI
-        )
+    # load labled KMeans data
+    labled_songs_df = methods.load_labled_spotify_songs()  # ["song_id"] -> lable. returns dataframe
 
-        sp = spotipy.Spotify(auth=token)
-        return sp
+    input_songs_audio_features = methods.get_songs_audio_features(song_id_list, spotipy_obj)   
+    average_song_features = input_songs_audio_features.mean()
+    average_song_cluster_label = kmeans_model.predict([average_song_features])
+    similar_song_id_list = methods.get_n_similar_songs(
+        average_song_cluster_label[0],
+        labled_songs_df,
+        recommendation_count,
+        spotipy_obj
+    )
 
-    def load_model():
-        import pickle
-        import sklearn
+    output_df = methods.get_songs_audio_features(similar_song_id_list, spotipy_obj)
 
-        dbfile = open('./kmeans.pickl', 'rb')
-        model = pickle.load(dbfile)
-        dbfile.close()
-        return model
-
-    def load_labled_spotify_songs():
-        return pd.read_csv("./labled_songs_id.csv")
-
-    def get_songs_audio_features(song_id_list, spotipy_obj):
-        temp_data = spotipy_obj.audio_features(song_id_list)
-        spotify_audio_features = pd.DataFrame(temp_data)
-
-        important_features = [
-            # "genre",  # spotify genre doesn't match with kaggle genre
-            # "popularity",  # don't know how to get it for the song, and it changes over time
-            # "key",  # This is categorical
-            # "mode",  # this is categorical
-            # "time_signature"  # not for now
-            "acousticness",
-            "danceability",
-            "energy",
-            "instrumentalness",
-            "liveness",
-            "loudness",
-            "speechiness",
-            "tempo",
-            "valence",
-        ]
-        return spotify_audio_features[important_features]
-
-    def get_n_similar_songs(song_label, labled_songs, n, spotipy_obj):
-        '''
-        args:
-            song_label: the average song audio features
-            labled_songs: the dataframe that has all the
-                songs' cluster lables
-            n: number of required recommendations
-        
-        return:
-            list of recommended song ids
-        '''
-
-        same_cluster_songs = \
-            labled_songs[labled_songs["label"] == song_label]\
-            .loc[:, "track_id"]
-
-        list_of_recommended_songs = same_cluster_songs.head(n).to_list()
-        return list_of_recommended_songs
-
-    def main(song_id_list, recommendation_count):
-        # get spotipy token
-        spotipy_obj = get_spotify_token()
-
-        # load ML model
-        kmeans_model = load_model()
-
-        # load labled KMeans data
-        labled_songs_df = load_labled_spotify_songs()  # ["song_id"] -> lable. returns dataframe
-
-        input_songs_audio_features = get_songs_audio_features(song_id_list, spotipy_obj)   
-        average_song_features = input_songs_audio_features.mean()
-        average_song_cluster_label = kmeans_model.predict([average_song_features])
-        similar_song_id_list = get_n_similar_songs(
-            average_song_cluster_label[0],
-            labled_songs_df,
-            recommendation_count,
-            spotipy_obj
-        )
-
-        output_df = get_songs_audio_features(similar_song_id_list, spotipy_obj)
-
-        return output_df
-
-    return main(song_id_list, recommendation_count)
+    return output_df
 
 
 if __name__ == "__main__":
